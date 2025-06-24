4c19a82afae867120d21ed903c97e8543125fcf7
Nurul Aisyah <nurulaisyahazman99@gmail.com>
2025-06-24 20:19:12 +0800

    Fixing and aligning with Mr Paul's codes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,55 +1,64 @@
-// import "./App.css
-import Welcome from "./Welcome";
+// import "./App.css"
+import { useState } from "react"
+import Welcome from "./Welcome"
 import { btnClick } from "./btnClick";
 import Profile from "./components/Profile";
 
 function App() {
   const myName = "Aisyah"
   const fullName = "Nurul Aisyah"
-  let fruits = ["Apple", "Mango", "Banana", "Kiwi"]
+  let fruits = ["Apple", "Mango", "Banana", "Kiwi", "Watermelon", "Orange", "Lemon"]
+
+  let count = 0
+
+  const increaseCount = () => {
+    count++
+    console.log(count)
+  }
+
+  let [stateCount, setStateCount] = useState(0)
 
   const profiles = [
     {
       name: "Paul",
       title: "Software Engineer",
-      imageUrl: "https://cdn.suitsupply.com/image/upload/ar_10:21,b_rgb:efefef,bo_200px_solid_rgb:efefef,c_pad,g_north,w_2600/b_rgb:efefef,c_lfill,g_north,dpr_1,w_768,h_922,f_auto,q_auto,fl_progressive/products/suits/default/Summer/P6932_1.jpg"
+      imageUrl: "https://images.unsplash.com/photo-1623880840102-7df0a9f3545b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG1hbiUyMGluJTIwc3VpdHxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
       name: "Tony Stark",
       title: "Genius Billionaire Philanthrophist",
-      imageUrl: "https://cdn.suitsupply.com/image/upload/ar_10:21,b_rgb:efefef,bo_200px_solid_rgb:efefef,c_pad,g_north,w_2600/b_rgb:efefef,c_lfill,g_north,dpr_1,w_768,h_922,f_auto,q_auto,fl_progressive/products/suits/default/Summer/P6932_1.jpg"
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDDgZHqmr2vZE83NizkSqV544kEpoCXnUBEw&s"
     },
     {
       name: "Hiccup",
       title: "Master of Dragons",
-      imageUrl: "https://cdn.suitsupply.com/image/upload/ar_10:21,b_rgb:efefef,bo_200px_solid_rgb:efefef,c_pad,g_north,w_2600/b_rgb:efefef,c_lfill,g_north,dpr_1,w_768,h_922,f_auto,q_auto,fl_progressive/products/suits/default/Summer/P6932_1.jpg"
+      imageUrl: "https://www.giantbomb.com/a/uploads/scale_medium/13/135472/1983080-hiccup_haddock.jpg"
     }
   ]
 
   return (
     <div>
-      <Welcome name={myName} fullNameABC={fullName} />
+      <Welcome name={myName} fullName={fullName}/>
       <h4>List of hobbies</h4>
       <ul>
         <li>Reading</li>
-        <li>Traveling</li>
-        <li>Gaming</li>
+        <li>Sport</li>
+        <li>Movies</li>
       </ul>
 
-      {myName == "Aisyah" ? <button onClick={() => btnClick(myName)}>It's Aisyah</button> : null}
+      {myName == "Aisyah" ? <button onClick={() => btnClick(myName)}>It's Aisyah!</button> : <h2>It's not Aisyah!</h2>}
       <ol>
         {fruits.map((fruit, i) => (
           <li key={i}>{fruit}</li>
         ))}
       </ol>
 
+      {/* {profiles.map((profile, i) => (
+        <Profile key={i} name={profile.name} title={profile.title} imageUrl={profile.imageUrl}/>
+      ))} */}
 
-      {profiles.map((profile, i) => (
-        <Profile key={i} name={profile.name} title={profile.title} imgUrl={placeholderUrl} />
-      ))}
-      <button onClick={() => count()}>Count</button>
+      <button onClick={() => setStateCount(stateCount + 1)}>Count: {stateCount}</button>
     </div>
   );
 }
-
-export default App;
+export default App
--- my-react-app/src/components/Profile.jsx
+++ my-react-app/src/components/Profile.jsx
@@ -3,9 +3,9 @@ function Profile ({name, title}) {
         border: '1px solid black',
         padding: '10px',
         width: "350px",
-        margin: '10px auto',
+        margin: '30px auto',
     }
-    const placeholderUrl = https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png
+    const placeholderUrl = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
     return (
         <div style={profileStyle}>
             <img width={300} src={imageUrl || placeholderUrl} alt="image" />
